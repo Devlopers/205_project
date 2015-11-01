c4f82fd1c8af5b8257dcfac9f3364aa0d1a35d58
Johnny Yeo <johnnyyeo12@gmail.com>
2015-10-31 20:44:39 -0700

    created a script for pushing stock data to directory of choice

---
+++ StockScraper-master/stock_pusher.py
@@ -0,0 +1,23 @@
+import stockretriever as st
+import json
+from datetime import datetime
+from stock_symbols import dj_30
+from sys import argv
+
+stock_info = st.get_current_info(dj_30)
+stock_data = {}
+for blob in range(len(stock_info)):
+    stock_data[blob] = {"symbol":{},"time":{},"price":{}}
+    stock_data[blob]["symbol"] = stock_info[blob]["Symbol"] 
+    stock_data[blob]["time"] = datetime.strptime(stock_info[blob]["LastTradeDate"]+" "+stock_info[blob]["LastTradeTime"], '%m/%d/%Y %I:%M%p')
+    stock_data[blob]["price"] = float(stock_info[blob]["LastTradePriceOnly"])
+
+def date_handler(obj):
+    return obj.isoformat() if hasattr(obj, 'isoformat') else obj
+
+out_path = argv[1] + "/stocks.txt"
+
+if __name__ == "__main__":
+    with open(out_path, 'w') as outfile:
+        print stock_data
+        json.dump(stock_data, outfile, sort_keys = True, indent = 4, ensure_ascii=False, default=date_handler)
